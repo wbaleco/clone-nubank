7a673abd8a9470d56f50c8c9e4b4333ce9fa5605
wbaleco <wbaleco@hotmail.com>
2020-10-14 22:26:59 -0300

    Enviando projeto para repositório

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  public options: Array<any> = [
+    {icon: 'person-add-outline', text:'Indicar amigos'},
+    {icon: 'phone-portrait-outline', text:'Racarga de celular'},
+    {icon: 'wallet-outline', text:'Depositar'},
+    {icon: 'options-outline', text:'Ajustar limite'},
+    {icon: 'help-circle-outline', text:'Me ajuda'},
+    {icon: 'barcode-outline', text:'Pagar'},
+    {icon: 'lock-open-outline', text:'Bloquear cartão'},
+    {icon: 'card-outline', text:'Cartão virtual'},
+    
+
+
+  ];
+
+  public slidesOptions: any = {slidesPerView: 3, freeMode: true};
+
+  public items: Array<any> = [
+    {icon: 'help-circle-outline', text:'Me ajuda'},
+    {icon: 'person-outline', text:'Perfil'},
+    {icon: 'cash-outline', text:'Configurar conta'},
+    {icon: 'card-outline', text:'Configurar cartão'},
+    {icon: 'phone-portrait-outline', text:'Configurações do app'},
+    
+    
+
+
+  ];
+
+  constructor() {}
+
+}
